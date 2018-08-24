98211aec347d81471ca3e69880e86c62d1be4eab
Dérick Hogan Pimenta <derickhoganpimenta@yahoo.com.br>
2018-08-24 12:49:06 -0300

    Change default port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
 
+const NEST_PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
-  await app.listen(3000);
+  await app.listen(NEST_PORT);
 }
 bootstrap();
