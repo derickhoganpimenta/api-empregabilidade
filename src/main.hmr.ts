5ad916cd27b6321d354606a1761fbf8f23f33319
Dérick Hogan Pimenta <derickhoganpimenta@yahoo.com.br>
2018-08-24 12:44:33 -0300

    Change default port

--- src/main.hmr.ts
+++ src/main.hmr.ts
@@ -3,9 +3,11 @@ import { ApplicationModule } from './app.module';
 
 declare const module: any;
 
+const NEST_PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
-  await app.listen(3000);
+  await app.listen(NEST_PORT);
 
   if (module.hot) {
     module.hot.accept();
