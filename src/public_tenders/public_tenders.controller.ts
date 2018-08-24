f04bfe8aef6607bf7ae34840d6bfe56e43a3fe41
Dérick Hogan Pimenta <derickhoganpimenta@yahoo.com.br>
2018-08-24 12:54:29 -0300

    Add public_tenders routes

--- src/public_tenders/public_tenders.controller.ts
+++ src/public_tenders/public_tenders.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { PublicTendersService } from './public_tenders.service';
 import { PublicTender } from './public_tender.entity';
+import { Candidate } from '../candidates/candidate.entity';
 
 @Controller('public_tenders')
 export class PublicTendersController {
@@ -9,5 +10,15 @@ export class PublicTendersController {
   @Get()
   async findAll(): Promise<PublicTender[]> {
     return await this.publicTendersService.findAll();
+  }
+
+  @Get(":document_number")
+  async findOne(@Param() params): Promise<PublicTender> {
+    return await this.publicTendersService.findOne(params.document_number);
+  }
+
+  @Get(":document_number/candidates")
+  async findBy(@Param() params): Promise<PublicTender> {
+    return await this.publicTendersService.findBy(params.document_number);
   }
 }
--- src/public_tenders/public_tenders.service.ts
+++ src/public_tenders/public_tenders.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { PublicTender } from './public_tender.entity';
+import { Candidate } from '../candidates/candidate.entity';
 
 @Injectable()
 export class PublicTendersService {
@@ -10,5 +11,13 @@ export class PublicTendersService {
 
   async findAll(): Promise<PublicTender[]> {
     return await this.publicTendersRepository.findAll<PublicTender>();
+  }
+
+  async findOne(document_number): Promise<PublicTender> {
+    return await this.publicTendersRepository.find({where: {document_number: document_number}});
+  }
+
+  async findBy(document_number): Promise<PublicTender> {
+    return await this.publicTendersRepository.find({include: [Candidate], where: {document_number: document_number}});
   }
 }
