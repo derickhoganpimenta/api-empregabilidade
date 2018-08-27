f1a775b8bea34f4b6bdf5886b0f15128326146eb
Dérick Hogan Pimenta <derickhoganpimenta@yahoo.com.br>
2018-08-27 15:58:59 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
+import * as cors from 'cors';
 
 const NEST_PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
+  app.use(cors());
   await app.listen(NEST_PORT);
 }
 bootstrap();
